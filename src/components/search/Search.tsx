563ef6499380fa482453af49e527ae4d784e5143
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-10-11 01:26:50 +0300

    Update Search.tsx

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -11,7 +11,7 @@ const Search = (props: Props) => {
 
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=500000&namePrefix=${inputValue}`,
+      `${GEO_API_URL}/cities?minPopulation=50000&namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
