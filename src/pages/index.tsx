eb9d2ab2bdcfdce0923254e08cb2291fa27d6a1a
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-10-14 15:14:17 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,10 @@ import { WEATHER_API_URL } from '../utils/weather-fetch-helpers';
 const Home = () => {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecastWeather, setForecastWeather] = useState(null);
+  const [isLoading, setIsLoading]=useState(false)
 
   const handleOnSearchChange = (searchData) => {
+    setIsLoading(true)
     const [lat, lon] = searchData.value.split(' ');
     const currentWeatherFetch = fetch(
       `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`
@@ -22,8 +24,10 @@ const Home = () => {
         const forecastResponse = await response[1].json();
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
         setForecastWeather({ city: searchData.label, ...forecastResponse });
+      setIsLoading(false)
       })
       .catch((error) => console.log(error));
+    
   };
 
   return (
@@ -31,6 +35,8 @@ const Home = () => {
       <div className="lg:w-5/12 sm:w-8/12 mx-auto mt-12">
         <Search onSearchChange={handleOnSearchChange} />
       </div>
+      {isLoading && (<p> Spinner </p>)}
+      {!isLoading && (
       <div className="flex  flex-col    lg:flex-row justify-evenly items-start w-full mx-auto my-12  ">
         <div className="w-6/12 flex justify-end items-start ">
           {currentWeather && <CurrentWeather data={currentWeather} />}
@@ -38,7 +44,7 @@ const Home = () => {
         <div className="w-6/12 flex justify-start items-start">
           {forecastWeather && <Forecast data={forecastWeather} />}
         </div>
-      </div>
+      </div>)}
     </div>
   );
 };
