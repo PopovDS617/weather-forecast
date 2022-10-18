ecc700b7cdcf5f059b0ba56537112ec0159533ef
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-10-18 14:59:05 +0300

    Update dev.tsx

--- dev.tsx
+++ dev.tsx
@@ -1,1 +1,13 @@
 
+ <AnimatePresence mode="wait" initial={true}>
+   <motion.div
+    initial={{ y: 10, opacity: 0 }}
+    animate={{ y: 0, opacity: 1 }}
+    transition={{ duration: 0.8, delay:0.1 }}
+               >
+     
+     </motion.div>
+   
+   
+   
+</AnimatePresence>
