8dde484b3d7100fa301dcce3144ee2f2335394bd
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-10-18 15:04:01 +0300

    Update dev.tsx

--- dev.tsx
+++ dev.tsx
@@ -1,5 +1,12 @@
 
- <AnimatePresence mode="wait" initial={true}>
+ const variants = {
+  hidden: { opacity: 0, x: 0, y: 20 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: 20 }
+};
+
+
+<AnimatePresence mode="wait" initial={true}>
    <motion.div
     initial={{ y: 10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
