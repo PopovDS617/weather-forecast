a89feab86e469a53c127f962e422006224befb56
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-10-18 15:01:00 +0300

    Update dev.tsx

--- dev.tsx
+++ dev.tsx
@@ -4,8 +4,9 @@
     initial={{ y: 10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     transition={{ duration: 0.8, delay:0.1 }}
+    exit: { opacity: 0, x: 0, y: 20 }
                >
-     
+    {children}
      </motion.div>
    
    
